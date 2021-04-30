0c5b7942d234e6524ac9ea3082fcc730c4ce499c
Clark <qianfeng.shen@gmail.com>
2021-04-29 21:07:01 -0400

    code cleaning

--- ip_repo/rtl/rifl/gt_wrapper/gt_cdc.sv
+++ ip_repo/rtl/rifl/gt_wrapper/gt_cdc.sv
@@ -11,41 +11,34 @@
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 `timescale 1ns/1ps
-module gt_cdc #
-(
-    parameter N_CHANNEL = 1
-)
-(
+module gt_cdc (
     input logic init_clk,
-    input logic tx_gt_clk,
-    input logic [N_CHANNEL-1:0] rx_gt_clk,
+    input logic txusrclk2,
+    input logic rxusrclk2,
     input logic rst,
     input logic tx_good_tx_synced,
-    input logic [N_CHANNEL-1:0] rx_good_rx_synced,
+    input logic rx_good_rx_synced,
     output logic tx_good_init_synced,
-    output logic [N_CHANNEL-1:0] rx_good_init_synced
+    output logic rx_good_init_synced
 );
     sync_signle_bit #(
         .N_STAGE   (5),
         .INPUT_REG (1)
     ) tx_good_init_sync_inst (
-        .clk_in  (tx_gt_clk),
+        .clk_in  (txusrclk2),
     	.clk_out (init_clk),
         .rst     (rst),
         .din     (tx_good_tx_synced),
         .dout    (tx_good_init_synced)
     );
-    genvar i;
-    for (i = 0; i < N_CHANNEL; i++) begin
-        sync_signle_bit #(
-            .N_STAGE   (5),
-            .INPUT_REG (1)
-        ) rx_good_init_sync_inst (
-            .clk_in  (rx_gt_clk[i]),
-    	    .clk_out (init_clk),
-            .rst     (rst),
-            .din     (rx_good_rx_synced[i]),
-            .dout    (rx_good_init_synced[i])
-        );
-    end
+    sync_signle_bit #(
+        .N_STAGE   (5),
+        .INPUT_REG (1)
+    ) rx_good_init_sync_inst (
+        .clk_in  (rxusrclk2),
+        .clk_out (init_clk),
+        .rst     (rst),
+        .din     (rx_good_rx_synced),
+        .dout    (rx_good_init_synced)
+    );
 endmodule
